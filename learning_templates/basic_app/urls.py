a9366ca3500faced55fb23ff1e924451cd0369f7
AlexGopal <127698347+AlexGopal@users.noreply.github.com>
2023-03-13 05:17:12 -0400

    Update urls.py

--- learning_templates/basic_app/urls.py
+++ learning_templates/basic_app/urls.py
@@ -6,5 +6,5 @@ from basic_app import views
 app_name = 'basic_app'
 
 urlpatterns = [
-    path('register/', views.register, name = 'register'),
+    path('relative/', views.relative, name = 'relative'),
 ]
